3a28c7832830bd7ebbbc6cece8e0d25fbd1214d6
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-10-29 13:17:30 +0800

    Version 1.1.1 优化解析器

--- src/main/java/cn/origincraft/magic/function/results/ObjectResult.java
+++ src/main/java/cn/origincraft/magic/function/results/ObjectResult.java
@@ -2,9 +2,8 @@ package cn.origincraft.magic.function.results;
 
 
 import cn.origincraft.magic.expression.functions.FunctionResult;
-import cn.origincraft.magic.expression.functions.MagicResult;
 
-public class ObjectResult extends FunctionResult implements MagicResult {
+public class ObjectResult extends FunctionResult{
     private final Object object;
 
     public ObjectResult(Object object) {
